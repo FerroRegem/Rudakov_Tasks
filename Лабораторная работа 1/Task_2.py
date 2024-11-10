3a4b5e70e90270907870304099656c67a3f36cf6
FerroRegem <igorrudakov051102@gmail.com>
2024-11-10 17:21:47 +0300

    Update Task_2.py

--- Лабораторная работа 1/Task_2.py
+++ Лабораторная работа 1/Task_2.py
@@ -1,10 +1,10 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-Inf_volume_Mb = 1.44  # Объем в Мб
-Inf_volume_B = Inf_volume_Mb * 1024 * 1024 # Объем в байтах
-Num_str = 100   # Количество страниц
-Num_strings = 50    # Количество строк на странице
-Num_char = 25   # Количество символов в строке
-Inf_char = 4    # Объем одного символа
-Num_books = int(Inf_volume_B // (Num_str * Num_strings * Num_char * Inf_char))
+inf_volume_Mb = 1.44  # Объем в Мб
+inf_volume_B = inf_volume_Mb * 1024 * 1024 # Объем в байтах
+num_str = 100   # Количество страниц
+num_strings = 50    # Количество строк на странице
+num_char = 25   # Количество символов в строке
+inf_char = 4    # Объем одного символа
+num_books = int(inf_volume_B // (num_str * num_strings * num_char * inf_char))
 
-print("Количество книг, помещающихся на дискету:", Num_books)
+print("Количество книг, помещающихся на дискету:", num_books)
